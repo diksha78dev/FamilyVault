7dd7284300cce1dec2bae7cc24cc5362ebdb4d7b
Diksha Dabhole <dikshadabhole78@gmail.com>
2026-03-24 10:57:25 +0530

    fix : format uploadAt to readable in JSON format

--- src/main/java/com/diksha/FamilyVault/Document.java
+++ src/main/java/com/diksha/FamilyVault/Document.java
@@ -1,8 +1,15 @@
 package com.diksha.FamilyVault;
 
-import jakarta.persistence.*;
-import lombok.Data;
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Data
@@ -19,5 +26,7 @@ public class Document {
     private String category;
     private String filePath;
     private String familyCode;
+    
+    @JsonFormat(pattern = "dd MMM yyyy , hh:mm:a")
     private LocalDateTime uploadAt;
 }
